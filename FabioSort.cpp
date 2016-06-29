791c5796efece4c4a0f102f5f1694116d5f9b800
Luca Ciarrocchi <lukedes96@yahoo.com>
2016-06-29 22:21:25 +0200

    Update FabioSort.cpp

--- FabioSort.cpp
+++ FabioSort.cpp
@@ -1,7 +1,7 @@
 void Ordina (int v[],int num){
  int appoggio;
- for (int i=0;i<n;i++){
-  for (int j=0;j<n;j++){
+ for (int i=0;i<num;i++){
+  for (int j=0;j<num;j++){
    if (v[i]<v[j]){
     appoggio=v[i];
     v[i]=v[j];
